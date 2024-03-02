3fdf985f73b638b93cf5c067878d19eabf613454
Abdullah Anli <anliabdullah08@gmail.com>
2024-03-02 20:13:00 +0300

    post service updated

--- src/main/java/SocialMedia/service/PostService.java
+++ src/main/java/SocialMedia/service/PostService.java
@@ -1,7 +1,22 @@
 package SocialMedia.service;
 
+import SocialMedia.entity.Post;
+import SocialMedia.repository.PostRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PostService {
+    private PostRepository  postRepository;
+
+    public PostService(PostRepository postRepository){
+        this.postRepository=postRepository;
+    }
+    public List<Post>getPostByUserId(Long userId){
+        return postRepository.findByUserId(userId);
+    }
+    public Post createdPost(Post post){
+        return postRepository.save(post);
+    }
 }
